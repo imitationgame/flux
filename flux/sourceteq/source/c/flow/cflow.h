2667dace844d2a444c0a292d5189aa26a9de1512
vauxhall <zero.griffin@gmail.com>
2016-02-09 13:10:57 -0600

    controller flow

--- flux/sourceteq/source/c/flow/cflow.h
+++ flux/sourceteq/source/c/flow/cflow.h
@@ -1,13 +1,5 @@
-//
-//  cflow.h
-//  flux
-//
-//  Created by zero on 2/9/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import "appdel.h"
 
-#import <Foundation/Foundation.h>
-
-@interface cflow : NSObject
+@interface cflow:UIViewController
 
 @end
