7fe714f51a7dd251f40829d39ec82e4e0066ade1
vauxhall <zero.griffin@gmail.com>
2016-02-09 20:06:06 -0600

    model protocol

--- flux/sourceteq/source/m/flow/mflowetttypespro.m
+++ flux/sourceteq/source/m/flow/mflowetttypespro.m
@@ -2,4 +2,21 @@
 
 @implementation mflowetttypespro
 
+#pragma mark -
+#pragma mark types protocol
+
+-(NSString*)labeltext
+{
+    NSString *string = NSLocalizedString(@"", nil);
+    
+    return string;
+}
+
+-(UIView*)overview
+{
+    UIView *view = [[UIView alloc] init];
+    
+    return view;
+}
+
 @end
