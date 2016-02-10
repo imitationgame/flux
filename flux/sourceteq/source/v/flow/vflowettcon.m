5486445d7ff7aa458e611446e3a24f2fa357a1b9
vauxhall <zero.griffin@gmail.com>
2016-02-10 10:11:45 -0600

    model

--- flux/sourceteq/source/v/flow/vflowettcon.m
+++ flux/sourceteq/source/v/flow/vflowettcon.m
@@ -5,12 +5,9 @@
 -(instancetype)init:(mflowett*)model
 {
     self = [super init:model];
-    
-    CGFloat width = 60;
-    CGFloat height = 60;
-    
-    [self setFrame:CGRectMake(0, 0, width, height)];
-    [self setCenter:CGPointMake(self.model.width, self.model.height)];
+
+    [self setFrame:CGRectMake(0, 0, self.model.width, self.model.height)];
+    [self setCenter:CGPointMake(self.model.x, self.model.y)];
     [self.layer setCornerRadius:self.model.width / 2.0];
     
     return self;
