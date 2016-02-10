629c23cf758a819962591c02014dad160d99815e
vauxhall <zero.griffin@gmail.com>
2016-02-10 16:05:33 -0600

    view

--- flux/sourceteq/source/m/flow/mflowlinesitem.h
+++ flux/sourceteq/source/m/flow/mflowlinesitem.h
@@ -1,5 +1,14 @@
 #import "appdel.h"
 
+@class vflowline;
+
 @interface mflowlinesitem:NSObject
+
+-(instancetype)init:(CGPoint)starting ending:(CGPoint)ending;
+-(vflowline*)generateview;
+
+@property(weak, nonatomic)vflowline *view;
+@property(nonatomic)CGPoint starting;
+@property(nonatomic)CGPoint ending;
 
 @end
--- flux/sourceteq/source/m/flow/mflowlinesitem.m
+++ flux/sourceteq/source/m/flow/mflowlinesitem.m
@@ -2,4 +2,24 @@
 
 @implementation mflowlinesitem
 
+-(instancetype)init:(CGPoint)starting ending:(CGPoint)ending
+{
+    self = [super init];
+    
+    self.starting = starting;
+    self.ending = ending;
+    
+    return self;
+}
+
+#pragma mark public
+
+-(vflowline*)generateview
+{
+    vflowline *view = [[vflowline alloc] init];
+    self.view = view;
+    
+    return view;
+}
+
 @end
