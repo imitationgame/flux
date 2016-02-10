4048b1d95e46c25ddb36c887f49c9db34113a16d
vauxhall <zero.griffin@gmail.com>
2016-02-09 19:36:56 -0600

    model

--- flux/sourceteq/source/v/flow/vflow.h
+++ flux/sourceteq/source/v/flow/vflow.h
@@ -1,10 +1,12 @@
 #import "appdel.h"
 
 @class mflow;
+@class vflowcatalogett;
 
 @interface vflow:UIScrollView
 
 @property(strong, nonatomic)mflow *model;
+@property(weak, nonatomic)vflowcatalogett *catalogett;
 @property(weak, nonatomic)UIView *container;
 @property(nonatomic)NSUInteger width;
 @property(nonatomic)NSUInteger height;
