3a47ab67609789b7a1c0c6f15fdb6d02416648bb
vauxhall <zero.griffin@gmail.com>
2016-02-10 09:04:24 -0600

    model

--- flux/sourceteq/source/m/flow/mflowett.h
+++ flux/sourceteq/source/m/flow/mflowett.h
@@ -1,5 +1,9 @@
 #import <UIKit/UIKit.h>
 
+@class vflowett;
+
 @interface mflowett:NSObject
+
+-(vflowett*)generateview;
 
 @end
--- flux/sourceteq/source/m/flow/mflowett.m
+++ flux/sourceteq/source/m/flow/mflowett.m
@@ -1,5 +1,15 @@
 #import "mflowett.h"
+#import "vflowett.h"
 
 @implementation mflowett
+
+#pragma mark public
+
+-(vflowett*)generateview
+{
+    vflowett *ett = [[vflowett alloc] init];
+    
+    return ett;
+}
 
 @end
