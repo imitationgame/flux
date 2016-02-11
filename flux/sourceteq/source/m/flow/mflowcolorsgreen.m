ead7765a762c3327610e165842c42245b271e105
vauxhall <zero.griffin@gmail.com>
2016-02-11 11:13:21 -0600

    colors

--- flux/sourceteq/source/m/flow/mflowcolorsgreen.m
+++ flux/sourceteq/source/m/flow/mflowcolorsgreen.m
@@ -1,13 +1,28 @@
-//
-//  mflowcolorsgreen.m
-//  flux
-//
-//  Created by zero on 2/11/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mflowcolorsgreen.h"
 
 @implementation mflowcolorsgreen
+
+#pragma mark colors protocol
+
+-(UIColor*)color
+{
+    UIColor *color = [UIColor greenColor];
+    
+    return color;
+}
+
+-(NSString*)name
+{
+    NSString *name = NSLocalizedString(@"flow_colors_green", nil);
+    
+    return name;
+}
+
+-(colortype)type
+{
+    colortype type = colortype_green;
+    
+    return type;
+}
 
 @end
--- flux/sourceteq/source/m/flow/mflowcolorsyellow.m
+++ flux/sourceteq/source/m/flow/mflowcolorsyellow.m
@@ -1,13 +1,28 @@
-//
-//  mflowcolorsyellow.m
-//  flux
-//
-//  Created by zero on 2/11/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mflowcolorsyellow.h"
 
 @implementation mflowcolorsyellow
+
+#pragma mark colors protocol
+
+-(UIColor*)color
+{
+    UIColor *color = [UIColor yellowColor];
+    
+    return color;
+}
+
+-(NSString*)name
+{
+    NSString *name = NSLocalizedString(@"flow_colors_yellow", nil);
+    
+    return name;
+}
+
+-(colortype)type
+{
+    colortype type = colortype_yellow;
+    
+    return type;
+}
 
 @end
