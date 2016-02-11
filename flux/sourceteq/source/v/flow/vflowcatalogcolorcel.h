018ff0384c91f41949152131b8fee507d22abe6f
vauxhall <zero.griffin@gmail.com>
2016-02-11 11:42:18 -0600

    views circle

--- flux/sourceteq/source/v/flow/vflowcatalogcolorcel.h
+++ flux/sourceteq/source/v/flow/vflowcatalogcolorcel.h
@@ -2,4 +2,6 @@
 
 @interface vflowcatalogcolorcel:UICollectionViewCell
 
+@property(weak, nonatomic)UIView *circle;
+
 @end
--- flux/sourceteq/source/v/flow/vflowcatalogcolorcel.m
+++ flux/sourceteq/source/v/flow/vflowcatalogcolorcel.m
@@ -8,6 +8,25 @@
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor clearColor]];
     
+    CGFloat margin = 2;
+    CGFloat width_margin = frame.size.width - (margin * 2);
+    CGFloat width_margin_2 = width_margin / 2.0;
+    
+    UIView *circle = [[UIView alloc] init];
+    [circle setUserInteractionEnabled:NO];
+    [circle setClipsToBounds:YES];
+    [circle setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [circle.layer setCornerRadius:width_margin_2];
+    self.circle = circle;
+    
+    [self addSubview:circle];
+    
+    NSDictionary *views = @{@"circle":circle};
+    NSDictionary *metrics = @{@"margin":@(margin)};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(margin)-[circle]-(margin)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(margin)-[circle]-(margin)-|" options:0 metrics:metrics views:views]];
+    
     return self;
 }
 
