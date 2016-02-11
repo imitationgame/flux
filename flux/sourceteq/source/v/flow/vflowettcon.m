2bf4e7740a5ddc672f8041059e3e087091e0978f
vauxhall <zero.griffin@gmail.com>
2016-02-11 14:53:22 -0600

    model

--- flux/sourceteq/source/v/flow/vflowettcon.m
+++ flux/sourceteq/source/v/flow/vflowettcon.m
@@ -8,7 +8,6 @@
 
     [self setFrame:CGRectMake(0, 0, self.model.width, self.model.height)];
     [self setCenter:CGPointMake(self.model.x, self.model.y)];
-    [self.layer setCornerRadius:self.model.width / 2.0];
     
     return self;
 }
