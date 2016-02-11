af1762e946ba826d73a8cac58813331c55068ef9
vauxhall <zero.griffin@gmail.com>
2016-02-10 22:23:58 -0600

    model flow ettl ist

---
+++ flux/sourceteq/source/m/flow/mflowettlist.m
@@ -0,0 +1,13 @@
+//
+//  mflowettlist.m
+//  flux
+//
+//  Created by zero on 2/10/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mflowettlist.h"
+
+@implementation mflowettlist
+
+@end
