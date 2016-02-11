53879ab09e83d42784109ed1e279ed1556c9d148
vauxhall <zero.griffin@gmail.com>
2016-02-11 10:44:03 -0600

    colors protocol

---
+++ flux/sourceteq/source/m/flow/mflowcolors.m
@@ -0,0 +1,13 @@
+//
+//  mflowcolors.m
+//  flux
+//
+//  Created by zero on 2/11/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mflowcolors.h"
+
+@implementation mflowcolors
+
+@end
---
+++ flux/sourceteq/source/m/flow/mflowcolorsprotocol.h
@@ -0,0 +1,13 @@
+//
+//  mflowcolorsprotocol.h
+//  flux
+//
+//  Created by zero on 2/11/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol mflowcolorsprotocol <NSObject>
+
+@end
