7f847a2f49105dde751a992d87902a404e1af6e1
vauxhall <zero.griffin@gmail.com>
2016-02-11 18:56:43 -0600

    ett con

--- flux/sourceteq/source/m/flow/mflowettcon.m
+++ flux/sourceteq/source/m/flow/mflowettcon.m
@@ -2,4 +2,17 @@
 
 @implementation mflowettcon
 
+-(instancetype)init:(mflow*)flow atpoint:(mflowpointsitem*)point
+{
+    self = [super init:flow atpoint:point];
+ 
+    self.viewclass = [vflowettcon class];
+    self.width = 150;
+    self.height = 150;
+    
+    [self.linesitemlist linedown];
+    
+    return self;
+}
+
 @end
