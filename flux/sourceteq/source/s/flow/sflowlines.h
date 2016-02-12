89dbc83200c717b488bf13b51cfa664b04b57471
vauxhall <zero.griffin@gmail.com>
2016-02-12 10:40:21 -0600

    strategies

--- flux/sourceteq/source/s/flow/sflowlines.h
+++ flux/sourceteq/source/s/flow/sflowlines.h
@@ -3,6 +3,12 @@
 
 @interface sflowlines:NSObject
 
++(void)linesleft;
++(void)linesright;
++(void)linesrandom;
+-(NSUInteger)count;
 -(id<sflowlinesprotocol>)item:(NSUInteger)index;
+
+@property(strong, nonatomic)NSArray *array;
 
 @end
--- flux/sourceteq/source/s/flow/sflowlines.m
+++ flux/sourceteq/source/s/flow/sflowlines.m
@@ -2,4 +2,42 @@
 
 @implementation sflowlines
 
++(void)linesleft
+{
+    
+}
+
++(void)linesright
+{
+    
+}
+
++(void)linesrandom
+{
+    
+}
+
+-(instancetype)init
+{
+    self = [super init];
+    
+    return self;
+}
+
+#pragma mark public
+
+-(NSUInteger)count
+{
+    NSUInteger count = self.array.count;
+    
+    return count;
+}
+
+-(id<sflowlinesprotocol>)item:(NSUInteger)index
+{
+    id<sflowlinesprotocol> item = self.array[index];
+    
+    return item;
+}
+
 @end
