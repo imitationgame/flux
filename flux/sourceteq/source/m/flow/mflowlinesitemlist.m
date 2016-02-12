6db971231510d82f3a381b08f3df529fddb941f4
vauxhall <zero.griffin@gmail.com>
2016-02-11 18:52:39 -0600

    line start and end

--- flux/sourceteq/source/m/flow/mflowlinesitemlist.m
+++ flux/sourceteq/source/m/flow/mflowlinesitemlist.m
@@ -15,6 +15,16 @@
     return self;
 }
 
+-(void)addlinestart:(CGPoint)linestart lineend:(CGPoint)lineend
+{
+    mflowlinesitem *linemodel = [[mflowlinesitem alloc] init:linestart ending:lineend];
+    vflowline *lineview = [linemodel generateview];
+    linemodel.startingett = self.flowett;
+    [self add:linemodel];
+    [self.flowett.flow.view.contentview addSubview:lineview];
+    [self.flowett.flow.view addpointatx:lineend.x y:lineend.y];
+}
+
 #pragma mark public
 
 -(NSUInteger)count
@@ -41,17 +51,17 @@
     CGPoint linestart = CGPointMake(self.flowett.x, self.flowett.y);
     CGPoint lineend = [self.flowett.flow.view.contentview linefrom:linestart deltax:0 deltay:1];
     
-    mflowlinesitem *linemodel = [[mflowlinesitem alloc] init:linestart ending:lineend];
-    vflowline *lineview = [linemodel generateview];
-    linemodel.startingett = self.flowett;
-    [self add:linemodel];
-    [self.flowett.flow.view.contentview addSubview:lineview];
-    [self.flowett.flow.view addpointatx:lineend.x y:lineend.y];
+    [self addlinestart:linestart lineend:lineend];
 }
 
 -(void)linesparting
 {
     CGPoint linestart = CGPointMake(self.flowett.x, self.flowett.y);
+    CGPoint lineendright = [self.flowett.flow.view.contentview linefrom:linestart deltax:1 deltay:0];
+    CGPoint lineendleft = [self.flowett.flow.view.contentview linefrom:linestart deltax:-1 deltay:0];
+    
+    [self addlinestart:linestart lineend:lineendright];
+    [self addlinestart:linestart lineend:lineendleft];
 }
 
 @end
