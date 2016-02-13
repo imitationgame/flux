98ebafc40cfd11df369aab44991429cd99b756f9
vauxhall <zero.griffin@gmail.com>
2016-02-13 09:48:57 -0600

    close

--- flux/sourceteq/source/v/flow/vflowett.m
+++ flux/sourceteq/source/v/flow/vflowett.m
@@ -13,13 +13,13 @@
     [self.imageView setClipsToBounds:YES];
     [self.imageView setTintColor:[model.color color]];
     [self.layer setBorderWidth:2];
-    [self hover];
     
     UIButton *button = [[UIButton alloc] init];
     [button setClipsToBounds:YES];
     [button setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [button setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
-    [button setImage:[UIImage imageNamed:@"close"] forState:UIControlStateHighlighted];
+    [button setImage:[[UIImage imageNamed:@"close"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:UIControlStateNormal];
+    [button setImage:[[UIImage imageNamed:@"close"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] forState:UIControlStateHighlighted];
+    [button setTintColor:[UIColor colorWithRed:1 green:0.2 blue:0 alpha:0.5]];
     [button.imageView setClipsToBounds:YES];
     [button.imageView setContentMode:UIViewContentModeScaleAspectFit];
     self.button = button;
@@ -32,8 +32,10 @@
     NSDictionary *views = @{@"button":button};
     NSDictionary *metrics = @{};
     
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[button(65)]-(-10)-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(-10)-[button(65)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[button(60)]-(0)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[button(60)]" options:0 metrics:metrics views:views]];
+    
+    [self hover];
     
     return self;
 }
@@ -59,12 +61,14 @@
         [self.layer setBorderColor:[UIColor colorWithWhite:0 alpha:0.8].CGColor];
         [self.imageView setAlpha:0.3];
         [self.button setHidden:NO];
+        [self setBackgroundColor:[UIColor whiteColor]];
     }
     else
     {
         [self.layer setBorderColor:[UIColor clearColor].CGColor];
         [self.imageView setAlpha:1];
         [self.button setHidden:YES];
+        [self setBackgroundColor:[UIColor clearColor]];
     }
 }
 
