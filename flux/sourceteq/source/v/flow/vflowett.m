b257e9d392243b5de3dde3c5e9110bf32b4b06b1
vauxhall <zero.griffin@gmail.com>
2016-02-13 09:43:06 -0600

    close

--- flux/sourceteq/source/v/flow/vflowett.m
+++ flux/sourceteq/source/v/flow/vflowett.m
@@ -18,10 +18,22 @@
     UIButton *button = [[UIButton alloc] init];
     [button setClipsToBounds:YES];
     [button setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [button setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
+    [button setImage:[UIImage imageNamed:@"close"] forState:UIControlStateHighlighted];
+    [button.imageView setClipsToBounds:YES];
+    [button.imageView setContentMode:UIViewContentModeScaleAspectFit];
     self.button = button;
     
     self.model = model;
     self.flowview = model.flow.view;
+    
+    [self addSubview:button];
+    
+    NSDictionary *views = @{@"button":button};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[button(65)]-(-10)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(-10)-[button(65)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
@@ -45,10 +57,14 @@
     if(self.isSelected || self.isHighlighted)
     {
         [self.layer setBorderColor:[UIColor colorWithWhite:0 alpha:0.8].CGColor];
+        [self.imageView setAlpha:0.3];
+        [self.button setHidden:NO];
     }
     else
     {
         [self.layer setBorderColor:[UIColor clearColor].CGColor];
+        [self.imageView setAlpha:1];
+        [self.button setHidden:YES];
     }
 }
 
