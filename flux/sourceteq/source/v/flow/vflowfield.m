4993f305b405aa868a1ca317a6899b93512d687a
vauxhall <zero.griffin@gmail.com>
2016-02-13 18:29:32 -0600

    field

--- flux/sourceteq/source/v/flow/vflowfield.m
+++ flux/sourceteq/source/v/flow/vflowfield.m
@@ -31,18 +31,38 @@
     [border setClipsToBounds:YES];
     [border setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    UIButton *button = [[UIButton alloc] init];
+    [button setClipsToBounds:YES];
+    [button setBackgroundColor:[UIColor clearColor]];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor colorWithWhite:0 alpha:0.1] forState:UIControlStateHighlighted];
+    [button.titleLabel setFont:[UIFont fontWithName:fontname size:15]];
+    [button setTitle:NSLocalizedString(@"flow_field_done", nil) forState:UIControlStateNormal];
+    [button setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [button addTarget:self action:@selector(actiondone:) forControlEvents:UIControlEventTouchUpInside];
+    
     [self addSubview:blur];
     [self addSubview:border];
+    [self addSubview:button];
     
-    NSDictionary *views = @{@"blur":blur, @"border":border};
+    NSDictionary *views = @{@"blur":blur, @"border":border, @"button":button};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[border]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[border(1)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[button(100)]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[button(40)]" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+#pragma mark actions
+
+-(void)actiondone:(UIButton*)button
+{
+    
 }
 
 @end
