1690fe91338df913c930e4fdf4ae2c6788b6ecf5
vauxhall <zero.griffin@gmail.com>
2016-02-15 11:46:57 -0600

    load view

--- flux/sourceteq/source/c/flow/cflowdetail.m
+++ flux/sourceteq/source/c/flow/cflowdetail.m
@@ -2,6 +2,23 @@
 
 @implementation cflowdetail
 
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self setTitle:NSLocalizedString(@"flow_detail_title", nil)];
+}
+
+-(void)loadView
+{
+    self.view = nil;
+}
+
 -(UIStatusBarStyle)preferredStatusBarStyle
 {
     return UIStatusBarStyleLightContent;
