316a97598b1a87c2f8a4bd856458edfafa6c779f
vauxhall <zero.griffin@gmail.com>
2016-02-15 13:53:50 -0600

    context

--- flux/sourceteq/source/c/flow/cflow.m
+++ flux/sourceteq/source/c/flow/cflow.m
@@ -18,21 +18,46 @@
 
 -(void)exportflow
 {
+    UIImage *image;
     vflowcontent *content = self.viewflow.contentview;
-    UIView *snapshot = [content.container snapshotViewAfterScreenUpdates:YES];
-    CGFloat x = snapshot.frame.origin.x;
-    CGFloat y = snapshot.frame.origin.y;
-    CGFloat width = content.contentSize.width;
-    CGFloat height = content.contentSize.height;
-    UIScrollView *scroll = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, width, height)];
-    [scroll setContentSize:CGSizeMake(width, height)];
-    [scroll addSubview:snapshot];
-    [scroll setBackgroundColor:[UIColor whiteColor]];
-    [snapshot setFrame:CGRectMake(-(CGFloat)content.marginleft, -(CGFloat)content.margintop, content.width, content.height)];
     
-    UIView *snapshot2 = [scroll snapshotViewAfterScreenUpdates:YES];
     
-    [self.navigationController pushViewController:[[cflowdetail alloc] init:snapshot2] animated:YES];
+    
+    
+    
+    
+    CGSize size = content.contentSize;
+    CGFloat width = size.width;
+    CGFloat marginleft = -(CGFloat)content.marginleft;
+    CGFloat margintop = -(CGFloat)content.margintop;
+    CGFloat totalwidth = content.width;
+    CGFloat totalheight = content.height;
+    UIGraphicsBeginImageContextWithOptions(size, YES, [UIScreen mainScreen].scale);
+    [content.container drawViewHierarchyInRect:CGRectMake(marginleft, margintop, totalwidth, totalheight) afterScreenUpdates:YES];
+    image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    NSString *filename = NSLocalizedString(@"flow_exportname", nil);
+    NSURL *url = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:filename]];
+    
+    NSError *error;
+    [UIImagePNGRepresentation(image) writeToURL:url options:NSDataWritingAtomic error:&error];
+    
+    if(error)
+    {
+        NSLog(@"error!!!");
+    }
+    
+    UIActivityViewController *act = [[UIActivityViewController alloc] initWithActivityItems:@[url] applicationActivities:nil];
+    
+    if([UIPopoverPresentationController class])
+    {
+        act.popoverPresentationController.sourceView = self.viewflow;
+        act.popoverPresentationController.sourceRect = CGRectMake((width / 2) - 2, 5, 1, 1);
+        act.popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirectionUp | UIPopoverArrowDirectionDown;
+    }
+    
+    [[cmain singleton] presentViewController:act animated:YES completion:nil];
 }
 
 -(void)share
