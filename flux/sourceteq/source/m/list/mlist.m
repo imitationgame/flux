68e33ea317f4bd4b94422e1269be3a4b2be44708
vauxhall <zero.griffin@gmail.com>
2016-02-15 20:41:03 -0600

    model

--- flux/sourceteq/source/m/list/mlist.m
+++ flux/sourceteq/source/m/list/mlist.m
@@ -15,7 +15,7 @@
 
 -(void)dealloc
 {
-    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 #pragma mark public
@@ -23,16 +23,33 @@
 -(void)reload
 {
     array = [NSMutableArray array];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),
+                   ^
+                   {
+                       NSString *query = @"SELECT id, created, path FROM flows ORDER BY created ASC;";
+                       NSArray *rawflows = [db rows:query];
+                       NSUInteger count = rawflows.count;
+                       
+                       for(NSUInteger i = 0; i < count; i++)
+                       {
+                           NSDictionary *rawflow = rawflows[i];
+                       }
+                   });
 }
 
 -(NSUInteger)count
 {
+    NSUInteger count = array.count;
     
+    return count;
 }
 
 -(mlistitem*)item:(NSUInteger)index
 {
+    mlistitem *item = array[index];
     
+    return item;
 }
 
 @end
