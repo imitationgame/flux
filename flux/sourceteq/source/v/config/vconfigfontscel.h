e3ec43185625db58a16ce23f827c66ad6701a4b3
vauxhall <zero.griffin@gmail.com>
2016-02-16 12:37:22 -0600

    views

--- flux/sourceteq/source/v/config/vconfigfontscel.h
+++ flux/sourceteq/source/v/config/vconfigfontscel.h
@@ -1,5 +1,10 @@
 #import "appdel.h"
+#import "mconfigfontsprotocol.h"
 
 @interface vconfigfontscel:UICollectionViewCell
+
+-(void)config:(id<mconfigfontsprotocol>)model;
+
+@property(weak, nonatomic)UILabel *label;
 
 @end
--- flux/sourceteq/source/v/config/vconfigfontscel.m
+++ flux/sourceteq/source/v/config/vconfigfontscel.m
@@ -2,4 +2,13 @@
 
 @implementation vconfigfontscel
 
+-(instancetype)
+
+#pragma mark public
+
+-(void)config:(id<mconfigfontsprotocol>)model
+{
+    
+}
+
 @end
