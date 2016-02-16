adb888d1f20d831ead4f095bc7d79d44776be5be
vauxhall <zero.griffin@gmail.com>
2016-02-16 12:35:07 -0600

    collection

--- flux/sourceteq/source/v/config/vconfigfonts.m
+++ flux/sourceteq/source/v/config/vconfigfonts.m
@@ -12,6 +12,12 @@
     self.model = [[mconfigfonts alloc] init];
     
     UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setHeaderReferenceSize:CGSizeZero];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:10];
+    [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    [flow setSectionInset:UIEdgeInsetsMake(0, 20, 0, 20)];
     
     UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
     [collection setClipsToBounds:YES];
@@ -30,6 +36,13 @@
 
 #pragma mark -
 #pragma mark col del
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
+{
+    CGSize size = CGSizeMake(170, col.bounds.size.height);
+    
+    return size;
+}
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
