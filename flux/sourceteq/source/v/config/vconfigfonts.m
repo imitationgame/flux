2c8727d4e13e962598a3a4f6f0c642615d72444c
vauxhall <zero.griffin@gmail.com>
2016-02-16 12:33:27 -0600

    collection

--- flux/sourceteq/source/v/config/vconfigfonts.m
+++ flux/sourceteq/source/v/config/vconfigfonts.m
@@ -11,7 +11,43 @@
 
     self.model = [[mconfigfonts alloc] init];
     
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setClipsToBounds:YES];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setShowsHorizontalScrollIndicator:NO];
+    [collection setShowsVerticalScrollIndicator:NO];
+    [collection setAlwaysBounceHorizontal:YES];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [collection setDelegate:self];
+    [collection setDataSource:self];
+    [collection registerClass:[vconfigfontscel class] forCellWithReuseIdentifier:celid];
+    [self addSubview:collection];
+    
     return self;
+}
+
+#pragma mark -
+#pragma mark col del
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView*)col numberOfItemsInSection:(NSInteger)section
+{
+    NSUInteger count = [self.model count];
+    
+    return count;
+}
+
+-(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
+{
+    vconfigfontscel *cel = [col dequeueReusableCellWithReuseIdentifier:celid forIndexPath:index];
+    
+    return cel;
 }
 
 @end
