81710afa63d2546f97aa952c4cc1cfea7eafc5af
vauxhall <zero.griffin@gmail.com>
2016-02-16 12:12:31 -0600

    fonts

---
+++ flux/sourceteq/source/m/config/mconfigfontsfutura.m
@@ -0,0 +1,13 @@
+//
+//  mconfigfontsfutura.m
+//  flux
+//
+//  Created by zero on 2/16/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mconfigfontsfutura.h"
+
+@implementation mconfigfontsfutura
+
+@end
---
+++ flux/sourceteq/source/m/config/mconfigfontstimes.m
@@ -0,0 +1,13 @@
+//
+//  mconfigfontstimes.m
+//  flux
+//
+//  Created by zero on 2/16/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mconfigfontstimes.h"
+
+@implementation mconfigfontstimes
+
+@end
