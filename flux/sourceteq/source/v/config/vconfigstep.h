8edcdf691377ab598d6a6e9892bf44722b7dbe76
vauxhall <zero.griffin@gmail.com>
2016-02-16 11:18:40 -0600

    stepper

--- flux/sourceteq/source/v/config/vconfigstep.h
+++ flux/sourceteq/source/v/config/vconfigstep.h
@@ -2,4 +2,8 @@
 
 @interface vconfigstep:UIView
 
+@property(weak, nonatomic)UILabel *label;
+@property(weak, nonatomic)UILabel *labelcount;
+@property(weak, nonatomic)UIStepper *stepper;
+
 @end
--- flux/sourceteq/source/v/config/vconfigstep.m
+++ flux/sourceteq/source/v/config/vconfigstep.m
@@ -9,6 +9,39 @@
     [self setBackgroundColor:[UIColor clearColor]];
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    UILabel *label = [[UILabel alloc] init];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setUserInteractionEnabled:NO];
+    [label setFont:[UIFont fontWithName:fontname size:16]];
+    [label setTextColor:[UIColor colorWithWhite:0 alpha:0.5]];
+    self.label = label;
+    
+    UILabel *labelcount = [[UILabel alloc] init];
+    [labelcount setBackgroundColor:[UIColor clearColor]];
+    [labelcount setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labelcount setUserInteractionEnabled:NO];
+    [labelcount setFont:[UIFont fontWithName:fontname size:18]];
+    [labelcount setTextColor:[UIColor colorWithWhite:0 alpha:0.8]];
+    [labelcount setTextAlignment:NSTextAlignmentRight];
+    self.labelcount = labelcount;
+    
+    UIStepper *stepper = [[UIStepper alloc] init];
+    [stepper setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [stepper setMinimumValue:11];
+    [stepper setMaximumValue:20];
+    self.stepper = stepper;
+    
+    [self addSubview:label];
+    [self addSubview:labelcount];
+    [self addSubview:stepper];
+    
+    NSDictionary *views = @{@"label":label, @"count":labelcount, @"stepper":stepper};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"" options:0 metrics:metrics views:views]];
+    
     return self;
 }
 
