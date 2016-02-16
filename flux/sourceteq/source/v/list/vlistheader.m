0cf90183ac1c1ff419041e32cd5cbbf566a5c347
vauxhall <zero.griffin@gmail.com>
2016-02-15 20:58:41 -0600

    model

--- flux/sourceteq/source/v/list/vlistheader.m
+++ flux/sourceteq/source/v/list/vlistheader.m
@@ -2,4 +2,30 @@
 
 @implementation vlistheader
 
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    UILabel *label = [[UILabel alloc] init];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setUserInteractionEnabled:NO];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setFont:[UIFont fontWithName:fontname size:20]];
+    [label setTextColor:[UIColor colorWithWhite:0 alpha:0.6]];
+    [label setTextAlignment:NSTextAlignmentCenter];
+    [label setNumberOfLines:0];
+    [label setText:NSLocalizedString(@"list_header_empty", nil)];
+    [self addSubview:label];
+    
+    NSDictionary *views = @{@"label":label};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[label]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[label]-0-|" options:0 metrics:metrics views:views]];
+    
+    return self;
+}
+
 @end
