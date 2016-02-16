d3e45eba705e1e761c2128928f99d4f035f7fec5
vauxhall <zero.griffin@gmail.com>
2016-02-16 09:44:21 -0600

    views

--- flux/sourceteq/source/v/config/vconfigfonts.m
+++ flux/sourceteq/source/v/config/vconfigfonts.m
@@ -2,6 +2,14 @@
 
 @implementation vconfigfonts
 
-
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    return self;
+}
 
 @end
