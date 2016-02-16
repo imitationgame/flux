55cdd66143ceaebbcb672d3db677cd6951e24d82
vauxhall <zero.griffin@gmail.com>
2016-02-16 10:34:05 -0600

    settings

--- flux/sourceteq/source/m/general/msettings.m
+++ flux/sourceteq/source/m/general/msettings.m
@@ -20,7 +20,17 @@
     
     if(settings)
     {
+        self.highresolution = [settings[@"hr"] boolValue];
+        self.fontselected = settings[@"fontname"];
+        self.fontsize = [settings[@"fontsize"] unsignedIntegerValue];
+    }
+    else
+    {
+        self.highresolution = YES;
+        self.fontselected = fontname;
+        self.fontsize = 15;
         
+        [self save];
     }
     
     return self;
@@ -31,7 +41,12 @@
 -(void)save
 {
     NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+    settings[@"hr"] = @(self.highresolution);
+    settings[@"fontname"] = self.fontselected;
+    settings[@"fontsize"] = @(self.fontsize);
+    
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setValue:settings forKey:@"settings"];
 }
 
 @end
