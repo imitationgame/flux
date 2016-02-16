ab2d00f828e4315f25cabe611f0621ea8f92e772
vauxhall <zero.griffin@gmail.com>
2016-02-16 12:23:55 -0600

    model

--- flux/sourceteq/source/m/config/mconfigfontsmenlo.m
+++ flux/sourceteq/source/m/config/mconfigfontsmenlo.m
@@ -2,4 +2,20 @@
 
 @implementation mconfigfontsmenlo
 
+#pragma mark public
+
+-(NSString*)name
+{
+    NSString *string = NSLocalizedString(@"", nil);
+    
+    return string;
+}
+
+-(NSString*)fontraw
+{
+    NSString *fontraw = fontname;
+    
+    return fontraw;
+}
+
 @end
