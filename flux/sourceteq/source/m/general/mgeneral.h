afa8f12d3c2fa95e998350dfe219c809413db5db
vauxhall <zero.griffin@gmail.com>
2016-03-16 16:17:00 -0600

    analytics font size

--- flux/sourceteq/source/m/general/mgeneral.h
+++ flux/sourceteq/source/m/general/mgeneral.h
@@ -36,7 +36,8 @@ typedef NS_ENUM(NSUInteger, ga_event)
     ga_event_rate,
     ga_event_email,
     ga_event_review,
-    ga_event_font
+    ga_event_font,
+    ga_event_fontsize
 };
 
 typedef NS_ENUM(NSUInteger, ga_action)
